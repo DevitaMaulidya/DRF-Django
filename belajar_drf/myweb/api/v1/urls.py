326884d054ac8011686f5a90b0e629fe0829b575
DevitaMaulidya <139763608+DevitaMaulidya@users.noreply.github.com>
2023-12-20 09:41:30 +0700

    Delete belajar_drf directory

--- belajar_drf/myweb/api/v1/urls.py
+++
@@ -1,7 +0,0 @@
-# from django.urls import path
-# from . import views
-# urlpatterns = [ path('items/', views.item_list, name='item-list'), ]
-
-from api.v1.routers import router
-
-urlpatterns = [] + router.urls
--- belajar_drf/myweb/apps/dashboard/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-
-urlpatterns = [
-#     path('admin/', admin.site.urls),
-#     path('dashboard/', include('apps.dashboard.urls')),
-]
